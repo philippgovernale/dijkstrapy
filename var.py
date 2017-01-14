f1853a38021452f0cdf67fd851ff746efdc44d18
Philipp Governale <philipp.governale1@gmail.com>
2017-01-14 15:55:57 +1300

    Fix incorrect command string in ADV_OPERATORS

--- var.py
+++ var.py
@@ -8,11 +8,13 @@ stack = []
 
 key = ''
 
+#to stack indicates whether a number should go to the stack. if it is false it should not
+tostack = True
+
 number = None
 keyword = None
 command = False
 decimal = False
-tostack = True
 comhelp = False
 lastkey = None
 helpcommand = None
@@ -22,7 +24,7 @@ decimal_insert = None
 SYS_COMMANDS = {'\r':sysfuncs.newline, '\b':sysfuncs.backspace, 'q':sysfuncs.quit, "'":sysfuncs.drop, ';':sysfuncs.clear_line, '?':sysfuncs.inline_help}
 
 OPERATORS = {"+":operator.add ,'-':operator.sub,'*':operator.mul,'/':operator.div}
-ADV_OPERATORS = {"pow":math.pow, 'srt':math.sqrt, 'cos':math.cos, 'sin':math.sin, "tan":math.tan, "log":math.log, "epow":math.exp, "tlog":math.log10, "!":math.factorial, "rm":sysfuncs.delete_stack, "nlog":math.log,"rad":math.radians, "deg":math.degrees, "crt":mathfuncs.cube_root, 'help':sysfuncs.help, 'man':sysfuncs.help,";":sysfuncs.clear_line}
+ADV_OPERATORS = {"pow":math.pow, 'srt':math.sqrt, 'cos':math.cos, 'sin':math.sin, "tan":math.tan, "log":math.log, "epow":math.exp, "tlog":math.log10, "!":math.factorial, "rm":sysfuncs.delete_stack, "nlog":math.log,"ra":math.radians, "deg":math.degrees, "crt":mathfuncs.cube_root, 'help':sysfuncs.help, 'man':sysfuncs.help,";":sysfuncs.clear_line}
 MATHS_CONSTANTS = {"pi":math.pi, "eu":math.e}
 
 operation_single = ["srt", "cos", "sin", "tan", "epow", "tlog", "!", "del", "nlog", "ra", "deg", "crt"]
