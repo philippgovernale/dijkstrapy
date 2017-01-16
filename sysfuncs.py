philippgovernale/dijkstrapy

commit 7c9c8f8e84cc33b9c8cf57dd1bc1a9521e8fdccb

Formatting, style and exception changes to comply with PEP8

sysfuncs.py
@@ -17,32 +17,32 @@ def delete_stack(): #rm command
         var.stack.append(var.number)
     try:
         a = var.stack.pop()
-    except:
+    except IndexError:
         screen.write("\nNeed a stack to remove\n")
-        raw_input("Press enter to continue...")
     else:
         try:
             del var.stack[a]
-        except:
+        except IndexError:
             screen.write("\nStack %r does not exist. Cannot remove\n"%(a))
-            raw_input("Press enter to continue")
     finally:
+        raw_input("Press enter to continue")
         screen.draw()
 
 def drop():
+    '''remove last stack'''
     try:
         var.stack.pop()
-    except:
+    except IndexError
         screen.write("\nCannot remove stack")
         raw_input("\nPress enter to continue...")
     finally:
         screen.draw()
 
-def help():
+def assist():
     '''show help manual'''
     screen.clear()
-    screen.write('''rPyn Documentation\n
-        rpyn is a reverse polish notation (rpn) calculator that intends to simulate the experience of such a
+    screen.write('''Dijkstrapy Documentation\n
+        Dijkstrapy is a reverse polish notation (rpn) calculator that intends to simulate the experience of such a
         calculator on pc.\n
         Simple operations:\n
         \t+\t| adds numbers {2}\n
@@ -80,9 +80,8 @@ def backspace():
         if var.keyword == '':
             var.keyword = None
     elif var.number is not None:
-        try:
-            var.number = int(str(var.number)[:-1])
-        except:
+        var.number = int(str(var.number)[:-1])
+        if var.number == '':
             var.number = None
     elif var.helpcommand is not None:
         var.helpcommand = var.helpcommand[:-1]
@@ -91,7 +90,7 @@ def backspace():
     elif var.lastkey == '.':
         var.decimal = False
 
-def quit():
+def leave():
     sys.exit()
 
 def catch_inline_help():
@@ -100,14 +99,14 @@ def catch_inline_help():
     screen.write('?')
 
 def inline_help():
-        screen.clear()
-        if var.helpcommand[1:] in var.ADV_OPERATORS:
-            screen.write(var.ADV_OPERATORS[var.helpcommand[1:]].__doc__)
-        elif var.helpcommand[1:] in var.OPERATORS:
-            screen.write(var.OPERATORS[var.helpcommand[1:]].__doc__)
-        raw_input('\nPress enter to continue')
-        screen.clear()
-        var.comhelp = False
-        var.helpcommand = None
-        var.keyword = None
-        var.number = None
+    screen.clear()
+    if var.helpcommand[1:] in var.ADV_OPERATORS:
+        screen.write(var.ADV_OPERATORS[var.helpcommand[1:]].__doc__)
+    elif var.helpcommand[1:] in var.OPERATORS:
+        screen.write(var.OPERATORS[var.helpcommand[1:]].__doc__)
+    raw_input('\nPress enter to continue')
+    screen.clear()
+    var.comhelp = False
+    var.helpcommand = None
+    var.keyword = None
+    var.number = None

mathfuncs.py
@@ -1,5 +1,4 @@
 import math
-import time
 
 def cube_root(operand):
     '''Return the cube root of x'''

stackop.py
@@ -10,7 +10,7 @@ def two_pop():
 def operate(operator):
     try:
         a = two_pop()
-    except:
+    except IndexError:
         screen.write("\nTwo numbers required in the stack to complete operation")
         raw_input()
         screen.clear()
@@ -25,7 +25,7 @@ def adv_operate_double(function):
         var.stack.append(float(var.number))
     try:
         a = two_pop()
-    except:
+    except IndexError:
         screen.write("\nTwo numbers required in the stack to complete operation")
         raw_input()
         screen.clear()
@@ -41,7 +41,7 @@ def adv_operate_double(function):
             raw_input()
             screen.clear()
         else:
-            var.stack.append(function(a[1], a[0]))
+            var.stack.append(result)
     finally:
         screen.draw()
 
@@ -50,7 +50,7 @@ def adv_operate_single(function):
         var.stack.append(float(var.number))
     try:
         a = var.stack.pop()
-    except:
+    except IndexError:
         screen.write("\nOne number required in the stack to complete operation")
         raw_input()
         screen.clear()
@@ -66,7 +66,7 @@ def adv_operate_single(function):
             raw_input()
             screen.clear()
         else:
-            var.stack.append(function(a))
+            var.stack.append(result)
     finally:
         screen.draw()
 

var.py
@@ -18,13 +18,51 @@ comhelp = False
 lastkey = None
 helpcommand = None
 
-SYS_COMMANDS = {'\r':sysfuncs.newline, '\b':sysfuncs.backspace, 'q':sysfuncs.quit, "'":sysfuncs.drop, ';':sysfuncs.clear_line, '?':sysfuncs.catch_inline_help}
+SYS_COMMANDS = {
+                '\r':sysfuncs.newline,
+                '\b':sysfuncs.backspace,
+                'q':sysfuncs.leave,
+                '\'':sysfuncs.drop,
+                ';':sysfuncs.clear_line,
+                '?':sysfuncs.catch_inline_help
+                }
 
-OPERATORS = {"+":operator.add ,'-':operator.sub,'*':operator.mul,'/':operator.div}
-ADV_OPERATORS = {"^":math.pow, 'srt':math.sqrt, 'cos':math.cos, 'sin':math.sin, "tan":math.tan, "log":math.log, "epow":math.exp, "tlog":math.log10, "!":math.factorial, "rm":sysfuncs.delete_stack, "nlog":math.log,"ra":math.radians, "deg":math.degrees, "crt":mathfuncs.cube_root, 'help':sysfuncs.help, 'man':sysfuncs.help,";":sysfuncs.clear_line, 'E':mathfuncs.sci_notation, '$':mathfuncs.invert_sign, 'dp':mathfuncs.decimal_places}
-MATHS_CONSTANTS = {"pi":math.pi, "eu":math.e}
+OPERATORS = {
+             '+':operator.add,
+             '-':operator.sub,
+             '*':operator.mul,
+             '/':operator.div
+             }
 
-operation_single = ["srt", "cos", "sin", "tan", "epow", "tlog", "!", "nlog", "ra", "deg", "crt", "$"]
-operation_double = ["^", "log", 'E', 'dp']
-operation_none = ["pi", "eu"]
-operation_custom = ["rm", 'help', ';', "man"]
+ADV_OPERATORS = {
+                 '^':math.pow,
+                 'srt':math.sqrt,
+                 'cos':math.cos,
+                 'sin':math.sin,
+                 'tan':math.tan,
+                 'log':math.log,
+                 'epow':math.exp,
+                 'tlog':math.log10,
+                 '!':math.factorial,
+                 'rm':sysfuncs.delete_stack,
+                 'nlog':math.log,
+                 'ra':math.radians,
+                 'deg':math.degrees,
+                 'crt':mathfuncs.cube_root,
+                 'help':sysfuncs.assist,
+                 'man':sysfuncs.assist,
+                 ';':sysfuncs.clear_line,
+                 'E':mathfuncs.sci_notation,
+                 '$':mathfuncs.invert_sign,
+                 'dp':mathfuncs.decimal_places
+                 }
+
+MATHS_CONSTANTS = {
+                   'pi':math.pi,
+                   'eu':math.e
+                   }
+
+operation_single = ['srt', 'cos', 'sin', 'tan', 'epow', 'tlog', '!', 'nlog', 'ra', 'deg', 'crt', '$']
+operation_double = ['^', 'log', 'E', 'dp']
+operation_none = ['pi', 'eu']
+operation_custom = ['rm', 'help', ';', 'man']

commit 5ddb2a1a8d4b668cca3e1cb31dab6799bd0c994f

Fix syntax error introduced in 7c9c8f8e84cc33b9c8cf57dd1bc1a9521e8fdccb

sysfuncs.py
@@ -3,7 +3,6 @@ import screen
 
 import sys
 
-
 def clear_line(): # ; command
     '''clear line (not including stacks)'''
     var.number = None
@@ -32,7 +31,7 @@ def drop():
     '''remove last stack'''
     try:
         var.stack.pop()
-    except IndexError
+    except IndexError:
         screen.write("\nCannot remove stack")
         raw_input("\nPress enter to continue...")
     finally: