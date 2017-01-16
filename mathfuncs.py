f3236eeb2ec41d6717784d4d79667697770d51ca
Philipp Governale <philipp.governale1@gmail.com>
2017-01-16 18:19:48 +1300

    Add scientific notation invert sign functions

--- mathfuncs.py
+++ mathfuncs.py
@@ -3,5 +3,14 @@ import math
 def cube_root(operand):
     '''Return the cube root of x'''
     power = 1 / 3.0
-    crt = math.pow(operand, power)
-    return crt
+    result = math.pow(operand, power)
+    return result
+
+def sci_notation(operand1, operand2):
+    '''scientific notation'''
+    multiplier = math.pow(10, operand2)
+    result = operand1 * multiplier
+    return result
+
+def invert_sign(operand):
+    return -operand
