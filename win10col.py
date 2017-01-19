00ea3322b490c28e2cb34075fae157c4f9495253
Philipp Governale <philipp.governale1@gmail.com>
2017-01-20 09:13:42 +1300

    Add initial colour coding

---
+++ win10col.py
@@ -0,0 +1,11 @@
+import ctypes
+
+def enable_VT():
+    kernel32 = ctypes.WinDLL('kernel32')
+    hStdOut = kernel32.GetStdHandle(-11)
+    mode = ctypes.c_ulong()
+    kernel32.GetConsoleMode(hStdOut, ctypes.byref(mode))
+    mode.value |= 4
+    kernel32.SetConsoleMode(hStdOut, mode)
+
+print '\033[31m' + "some red text"
