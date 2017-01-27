3a3d3228a219095577a3e3d0b278a2fa84f1ab17
Philipp Governale <philipp.governale1@gmail.com>
2017-01-27 17:23:54 +1300

    Add round to significant figures function

--- mathfuncs.py
+++ mathfuncs.py
@@ -1,4 +1,5 @@
 import math
+from decimal import *
 
 def cube_root(operand):
     '''Return the cube root of x'''
@@ -19,4 +20,26 @@ def invert_sign(operand):
 def decimal_places(operand1, operand2):
     precision = int(operand2)
     result = round(operand1, precision)
+    return result
+
+def rnd(operand1, operand2):
+    num_before_dot = len(str(operand1).split('.')[0])
+    operand1 = float(operand1)
+    operand2 = int(operand2)
+    if num_before_dot +1 < operand2:
+        num_after_dot = operand2 - num_before_dot
+        round_str = '0.'
+        for dig in range(num_after_dot -1):
+            round_str += '0'
+        round_str += '1'
+        result= float(Decimal(operand1).quantize(Decimal(round_str), rounding=ROUND_HALF_UP))
+    elif num_before_dot == operand2:
+        result = int(Decimal(operand1).quantize(Decimal('1'),rounding=ROUND_HALF_UP))
+    else:
+        a = "%.*e" %(operand2-1, operand1)
+        if num_before_dot < operand2:
+            result = float(a)
+        else:
+            f = float(a)
+            result = int(f)
     return result
